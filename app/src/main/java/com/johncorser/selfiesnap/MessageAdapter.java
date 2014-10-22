5f33d06efdc43ef93615f1c8d343bcb2cc5afb44
John Corser <john@johncorser.com>
2014-10-22 16:37:07 -0400

    add timestamp to messages

--- app/src/main/java/com/johncorser/selfiesnap/MessageAdapter.java
+++ app/src/main/java/com/johncorser/selfiesnap/MessageAdapter.java
@@ -1,6 +1,7 @@
 package com.johncorser.selfiesnap;
 
 import android.content.Context;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 
 import com.parse.ParseObject;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -36,6 +38,7 @@ public class MessageAdapter extends ArrayAdapter<ParseObject> {
             holder = new ViewHolder();
             holder.iconImageView = (ImageView)convertView.findViewById(R.id.messageIcon);
             holder.nameLabel = (TextView)convertView.findViewById(R.id.senderLabel);
+            holder.timeLabel = (TextView)convertView.findViewById(R.id.timeLabel);
             convertView.setTag(holder);
         }
         else {
@@ -44,13 +47,22 @@ public class MessageAdapter extends ArrayAdapter<ParseObject> {
 
         ParseObject message = mMessages.get(position);
 
-        if (message.getString("fileType").equals("image")) {
-            holder.iconImageView.setImageResource(R.drawable.ic_action_picture);
+        Date createdAt = message.getCreatedAt();
+        long now = new Date().getTime();
+        String convertedDate = DateUtils.getRelativeTimeSpanString(
+                createdAt.getTime(),
+                now,
+                DateUtils.SECOND_IN_MILLIS).toString();
+
+        holder.timeLabel.setText(convertedDate);
+
+        if (message.getString(ParseConstants.KEY_FILE_TYPE).equals(ParseConstants.TYPE_IMAGE)) {
+            holder.iconImageView.setImageResource(R.drawable.ic_picture);
         }
         else {
-            holder.iconImageView.setImageResource(R.drawable.ic_action_play_over_video);
+            holder.iconImageView.setImageResource(R.drawable.ic_video);
         }
-        holder.nameLabel.setText(message.getString("senderName"));
+        holder.nameLabel.setText(message.getString(ParseConstants.KEY_SENDER_NAME));
 
         return convertView;
     }
@@ -58,11 +70,13 @@ public class MessageAdapter extends ArrayAdapter<ParseObject> {
     private static class ViewHolder {
         ImageView iconImageView;
         TextView nameLabel;
+        TextView timeLabel;
     }
 
-    public void refill(List<ParseObject> messages){
+    public void refill(List<ParseObject> messages) {
         mMessages.clear();
         mMessages.addAll(messages);
         notifyDataSetChanged();
     }
 }
+
